aad114003a514a59e7e135f53dbd62b4a6b7b82b
Azze-el arab Bouferdou <azzelarab.bouferdou.co@gmail.com>
2024-03-19 00:22:46 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
-    for smatrix in matrix:
-        if len(smatrix) == 0:
-            print()
-        for num in range(len(smatrix)):
-            print("{:d}".format(smatrix[num]),
-                end="\n" if num is len(smatrix) - 1 else " ")
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
