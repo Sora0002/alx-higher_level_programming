a5bdcb1d925a183ebe310806132edbbc9e322cd1
Azze-el arab Bouferdou <azzelarab.bouferdou.co@gmail.com>
2024-03-19 00:20:32 +0000

    test

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,72 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to list head
- * Return: 0 if it is a palindrome
- * 1 if it is a palidrome
-*/
-
-/*check if the list is empty or has only element*/
-int is_palindrome(listint_t **head)
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (find_palindrome(head, *head));
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
 }
 
 /**
- * find_palindrome - check if is it a palindrome
- * @head: pointer to the head of the list
- * @end: list end
- * Return: pointer to new head of the reversed list
-*/
-
-listint_t find_palindrome(listint_t **head, listint_t *end)
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
+ */
+int is_palindrome(listint_t **head)
 {
-	if (end == NULL)
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	if (find_palindrome(head, end->next) && (*head)->n == end->n)
+	while (1)
 	{
-		*head = (*head)->next;
-		return (1);
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
